e1cca1f8583c3cb84fb9d0d55c4a0dce06a08cce
hbkooo <1921642379@qq.com>
2021-06-04 10:15:00 +0800

    rm build dir

--- cmake-build-debug-visual-studio2017/CMakeFiles/NIMQtClient_autogen.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/NIMQtClient_autogen"
-  "NIMQtClient_autogen/mocs_compilation.cpp"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/NIMQtClient_autogen.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
